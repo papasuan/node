63c613e8e1be568b84658ab22fca4cdb4250466e
papasuan <scy.lamborghini@aliyun.com>
2016-09-22 14:19:32 +0800

    Add files via upload

---
+++ node1.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var items = [];
+
+var server = http.createServer(function(req , res){
+         if('/' == req.url){
+         	switch (req.method){
+         		case 'GET':
+         		 show(res);
+         		 break;
+
+         		 case 'POST':
+         		 add(req , res);
+         		 break;
+
+         		 default:
+         		 badRequest(res);
+         	}
+         }else{
+         	notFound(res);
+         }
+});
+
+server.listen(3000);
+
+
+function show(res) {
+	  var html = '<html><head><title>asdasd</title></head><body>'
+	           + '<h1>todo list</h1>'
+	           + '<ul>'
+	           + items.map(function(item){
+	           	    return '<li>' +item+ '</li>'
+	           }).join('')
+	           + '</ul>'
+	           + '<form method = 'POST' action = '/'>'
+	           + '<p><input type = 'text' name = 'item'/></p>'
+	           + '<p><input type = 'submit' value = 'ADD'/></p>'
+	           + '</form></body></html>';
+
+	           res.setHeader('Content-Type' , 'text/html');
+	           res.setHeader('Content-Type' , Buffer.byteLength(html));
+	           res.end(html);
+}
+
+
+var qs = require('querystring');
+
+function add(req , res){
+	  var body = '';
+	  req.setEncoding('utf-8');
+	  req.on('data' , function(chunk){body += chunk});
+	  req.on('end' , function(){
+	  	  var obj = qs.parse(body);
+	  	  items.push(obj.item);
+	  	  show(res);
+	  });
+}
